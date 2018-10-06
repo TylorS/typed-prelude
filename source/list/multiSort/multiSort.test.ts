c31537020b27661f910b57fbf47d394f79142eae
Tylor Steinberger <tylor@tryfynd.com>
2018-10-05 20:15:39 -0400

    feat: implement multiSort function
    Allows sorting lists within lists

---
+++ source/list/multiSort/multiSort.test.ts
@@ -0,0 +1,45 @@
+import { describe, given, it, Test } from '@typed/test'
+import { multiSort } from './multiSort'
+
+export const test: Test = describe(`multiSort`, [
+  given(`a list of sorting functions and a list`, [
+    it(`returns a sorted list`, ({ equal }) => {
+      type ABC = { a: number; b: number; c: number }
+      const values: ABC[] = [
+        { a: 1, b: 3, c: 1 },
+        { a: 1, b: 2, c: 1 },
+        { a: 1, b: 1, c: 1 },
+        { a: 1, b: 2, c: 2 },
+        { a: 1, b: 3, c: 2 },
+        { a: 1, b: 2, c: 3 },
+        { a: 1, b: 1, c: 2 },
+        { a: 2, b: 2, c: 2 },
+        { a: 2, b: 2, c: 1 },
+        { a: 2, b: 3, c: 1 },
+        { a: 2, b: 1, c: 1 },
+        { a: 2, b: 3, c: 2 },
+        { a: 2, b: 2, c: 3 },
+        { a: 2, b: 1, c: 2 },
+      ]
+      const actual = multiSort([x => x.a, x => x.b, x => x.c], values)
+      const expected: ABC[] = [
+        { a: 1, b: 1, c: 1 },
+        { a: 1, b: 1, c: 2 },
+        { a: 1, b: 2, c: 1 },
+        { a: 1, b: 2, c: 2 },
+        { a: 1, b: 2, c: 3 },
+        { a: 1, b: 3, c: 1 },
+        { a: 1, b: 3, c: 2 },
+        { a: 2, b: 1, c: 1 },
+        { a: 2, b: 1, c: 2 },
+        { a: 2, b: 2, c: 1 },
+        { a: 2, b: 2, c: 2 },
+        { a: 2, b: 2, c: 3 },
+        { a: 2, b: 3, c: 1 },
+        { a: 2, b: 3, c: 2 },
+      ]
+
+      equal(expected, actual)
+    }),
+  ]),
+])
---
+++ source/list/multiSort/multiSort.ts
@@ -0,0 +1,24 @@
+import { Arity1, id } from '../../lambda'
+import { ascend } from '../ascend'
+import { chain } from '../chain'
+import { groupBy } from '../groupBy'
+
+export function multiSort<A>(sortFns: Array<Arity1<A, PropertyKey>>, list: A[]): A[] {
+  if (sortFns.length === 0 || list.length === 0) {
+    return list
+  }
+
+  const initialObject = groupBy(sortFns[0], list)
+  const initialKeys = Object.keys(initialObject).sort(ascend(id))
+  const innerSortFns = sortFns.slice(1)
+  const result = initialKeys.reduce(
+    (acc, key) => {
+      acc[key] = multiSort(innerSortFns, initialObject[key])
+
+      return acc
+    },
+    {} as Record<string, A[]>,
+  )
+
+  return chain(x => result[x], initialKeys)
+}
