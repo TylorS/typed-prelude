962c2a5a06a1ea0ba4cf073cfcc51c039759d8a7
Tylor Steinberger <tylor@tryfynd.com>
2018-10-18 22:17:27 -0400

    feat: improve type signatures

--- source/math/divide.ts
+++ source/math/divide.ts
@@ -1,4 +1,10 @@
 import { curry } from '../lambda'
 
-export const divide = curry((left: number, right: number) => left / right)
-export const divideBy = curry((right: number, left: number) => left / right)
+export const divide: {
+  (left: number, right: number): number
+  (left: number): (right: number) => number
+} = curry((left: number, right: number) => left / right)
+export const divideBy: {
+  (right: number, left: number): number
+  (right: number): (left: number) => number
+} = curry((right: number, left: number): number => left / right)
