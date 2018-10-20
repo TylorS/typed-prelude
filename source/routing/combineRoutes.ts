610d75572e36b75639028c4d056692b8da833346
Tylor Steinberger <tylor@tryfynd.com>
2018-10-20 08:37:30 -0400

    feat: export useful type

--- source/routing/combineRoutes.ts
+++ source/routing/combineRoutes.ts
@@ -7,4 +7,4 @@ export const combineRoutes = <A extends Routes>(...routes: A): CombinedRoute<A> 
   createRoute(pathJoin(routes.map(x => x.path)))
 
 export type CombinedRoute<A extends Array<Route<any>>> = Route<CombinedParams<A>>
-type CombinedParams<A extends Routes> = And<{ [K in keyof A]: RouteParams<A[K]> }>
+export type CombinedParams<A extends Routes> = And<{ [K in keyof A]: RouteParams<A[K]> }>
