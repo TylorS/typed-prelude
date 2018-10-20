317ef29ea44208f3e5bcdc852153de71ebfa24c3
Tylor Steinberger <tylor@tryfynd.com>
2018-10-20 08:21:46 -0400

    fix: properly represent returned type

--- source/routing/nestMatch.ts
+++ source/routing/nestMatch.ts
@@ -2,24 +2,23 @@ import { Href } from '../history'
 import { curry } from '../lambda'
 import { Match } from '../logic'
 import { combine, Maybe } from '../maybe'
-import { MergeObjects } from '../objects'
+import { Overwrite } from '../objects'
 import { stripRouteFromPath } from './stripRouteFromPath'
 import { Route } from './types'
 
 export const nestMatch = curry(__nestMatch) as {
-  <A, B>(match: Match<Href, A>, route: Route<B>): Match<Href, MergeObjects<B, A>>
-  <A>(match: Match<Href, A>): <B>(route: Route<B>) => Match<Href, MergeObjects<B, A>>
+  <A, B>(match: Match<Href, A>, route: Route<B>): Match<Href, Overwrite<B, A>>
+  <A>(match: Match<Href, A>): <B>(route: Route<B>) => Match<Href, Overwrite<B, A>>
 }
 
 export function __nestMatch<A, B>(
   match: Match<Href, A>,
   route: Route<B>,
-): Match<Href, MergeObjects<B, A>> {
-  return (href: Href): Maybe<MergeObjects<B, A>> => {
+): Match<Href, Overwrite<B, A>> {
+  return (href: Href): Maybe<Overwrite<B, A>> => {
+    const maybeA = match(stripRouteFromPath(href, route))
     const maybeB = route.match(href)
-    const pathname = stripRouteFromPath(href, route)
-    const maybeA = match(pathname)
 
-    return combine((a, b) => (Object.assign({}, b, a) as any) as MergeObjects<B, A>, maybeA, maybeB)
+    return combine((a, b) => (Object.assign({}, b, a) as any) as Overwrite<B, A>, maybeA, maybeB)
   }
 }
