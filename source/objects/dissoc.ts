5bfefa1d0bda275ac90c314569cd5264cb03cd23
Tylor Steinberger <tylor@tryfynd.com>
2018-11-24 17:58:49 -0500

    feat: implement dissoc

---
+++ source/objects/dissoc.ts
@@ -0,0 +1,16 @@
+import { curry } from '../lambda'
+import { clone } from './clone'
+import { DropKeys } from './types'
+
+export const dissoc = curry(_dissoc) as {
+  <K extends PropertyKey, A extends Record<K, any>>(key: K, obj: A): DropKeys<A, K>
+  <K extends PropertyKey>(key: K): <A extends Record<K, any>>(obj: A) => DropKeys<A, K>
+}
+
+function _dissoc<K extends PropertyKey, A extends Record<K, any>>(key: K, obj: A): DropKeys<A, K> {
+  const cloned = clone(obj)
+
+  delete cloned[key]
+
+  return (cloned as any) as DropKeys<A, K>
+}
