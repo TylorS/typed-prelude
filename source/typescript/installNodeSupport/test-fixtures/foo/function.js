94affbb2196a140484fc2ad7eeab571f3670a4e2
Tylor Steinberger <tylor@tryfynd.com>
2019-01-25 22:49:31 -0500

    fix: remove build files

--- source/typescript/installNodeSupport/test-fixtures/foo/function.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function foo() {
-    return 'foo';
-}
-exports.foo = foo;
-//# sourceMappingURL=function.js.map
--- source/typescript/installNodeSupport/test-fixtures/foobar.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var function_1 = require("bar/function");
-var function_2 = require("foo/function");
-function foobar() {
-    return function_2.foo() + function_1.bar();
-}
-exports.foobar = foobar;
-//# sourceMappingURL=foobar.js.map
