539860c14f88111c17dd58edaa15c73686881d46
Tylor Steinberger <tylor@tryfynd.com>
2019-03-05 11:32:52 -0500

    feat: generateTestsArray

--- source/testing/generateTestBundle/generateTestsArray.test.ts
+++ source/testing/generateTestBundle/generateTestsArray.test.ts
@@ -3,8 +3,9 @@ import { generateTestsArray } from './generateTestsArray'
 
 export const test = describe(`generateTestsArray`, [
   given(`a number of tests`, [
-    it(`returns an array of of tests`, ({ equal }) => {
-      equal('[test0,test1,test2,test3]', generateTestsArray(4))
+    it(`returns an array of tests`, ({ equal }) => {
+      equal('const tests = [test0, test1, test2, test3];', generateTestsArray(4))
+      equal('const allTests = [test0, test1, test2, test3];', generateTestsArray(4, 'allTests'))
     }),
   ]),
 ])
--- source/testing/generateTestBundle/generateTestsArray.ts
+++ source/testing/generateTestBundle/generateTestsArray.ts
@@ -1,11 +1,11 @@
-import { join, range } from '@typed/list'
+import { pipe } from '@typed/lambda'
+import { join, map, range } from '@typed/list'
 
-export function generateTestsArray(numberOfTests: number): string {
-  return join(' ', [
-    '[',
-    range(0, numberOfTests)
-      .map(i => `test${i}`)
-      .join(','),
-    ']',
-  ])
-}
+const createTests = pipe(
+  range(0),
+  map(i => `test${i}`),
+  join(', '),
+)
+
+export const generateTestsArray = (numberOfTests: number, variableName: string = 'tests'): string =>
+  join('', [`const ${variableName} = [`, createTests(numberOfTests), '];'])
