9c30e1eea5a1e827760d85e4cf4f6aa16905bb2d
Tylor Steinberger <tylor@tryfynd.com>
2019-03-04 22:15:54 -0500

    fix: improve withPrevious

--- source/lambda/withPrevious.ts
+++ source/lambda/withPrevious.ts
@@ -3,20 +3,31 @@ import { Arity1, Fn } from './types'
 
 const NOTHING = Symbol('nothing')
 type NOTHING = typeof NOTHING
+type Nothing = { [NOTHING]: true }
+type Just<A> = { [NOTHING]: false; value: A }
+type Maybe<A> = Just<A> | Nothing
 
 export const withPrevious = curry(function withPrevious<A, Args extends any[]>(
   withFn: Arity1<A, void>,
   fn: Fn<Args, A>,
 ): Fn<Args, A> {
-  let previous: A | NOTHING = NOTHING
+  let previous: Maybe<A> = { [NOTHING]: true }
   return (...args: Args): A => {
-    if (previous !== NOTHING) {
-      withFn(previous)
+    if (!isNothing(previous)) {
+      withFn(previous.value)
     }
 
-    return (previous = fn(...args))
+    const value = fn(...args)
+
+    previous = { [NOTHING]: false, value }
+
+    return value
   }
 }) as {
   <A, Args extends any[]>(withFn: Arity1<A>, fn: Fn<Args, A>): Fn<Args, A>
   <A>(withFn: Arity1<A>): <Args extends any[]>(fn: Fn<Args, A>) => Fn<Args, A>
+}
+
+function isNothing<A>(maybe: Maybe<A>): maybe is Nothing {
+  return maybe[NOTHING]
 }
