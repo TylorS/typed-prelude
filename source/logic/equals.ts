6048f2fcfd9fdd56109df712aca600dea868fb2e
Tylor Steinberger <tylor@tryfynd.com>
2019-03-31 02:37:37 -0400

    feat: equalsBy

--- source/logic/equals.ts
+++ source/logic/equals.ts
@@ -1,4 +1,4 @@
-import { curry } from '@typed/lambda'
+import { Arity1, curry } from '@typed/lambda'
 import { equals as _equals } from '../common/equals'
 
 export const equals = curry(<A>(a: A, b: A) => _equals(a, b, [], [])) as Equals
@@ -7,3 +7,12 @@ export type Equals = {
   <A, B = A>(a: A, b: B): B extends A ? boolean : false
   <A>(a: A): <B = A>(b: B) => B extends A ? boolean : false
 }
+
+export const equalsBy: {
+  <A, B>(by: Arity1<A, B>, a: A, b: A): boolean
+  <A, B>(by: Arity1<A, B>, a: A): (b: A) => boolean
+  <A, B>(by: Arity1<A, B>): {
+    (a: A, b: A): boolean
+    (a: A): (b: A) => boolean
+  }
+} = curry(<A, B>(by: Arity1<A, B>, a: A, b: A): boolean => equals(by(a), by(b)))
