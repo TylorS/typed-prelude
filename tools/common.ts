2991bca176e8a49b36c01b9f18487e5679b27356
Tylor Steinberger <tylor@tryfynd.com>
2019-05-04 17:17:07 -0400

    chore: only needed information & auto-find package

--- tools/common.ts
+++ tools/common.ts
@@ -1,72 +1,10 @@
-import { readFileSync } from 'fs'
+import { existsSync, readdirSync, statSync } from 'fs'
 import { join } from 'path'
-import {
-  convertCompilerOptionsFromJson,
-  Diagnostic,
-  flattenDiagnosticMessageText,
-  parseConfigFileTextToJson,
-} from 'typescript'
-
-export const PACKAGES = [
-  'common',
-  'lambda',
-  'disposable',
-  'new-type',
-  'strings',
-  'timer',
-  'list',
-  'subscription',
-  'tuple',
-  'either',
-  'maybe',
-  'env',
-  'loadable',
-  'promises',
-  'history',
-  'http',
-  'logger',
-  'logic',
-  'math',
-  'routing',
-  'storage',
-  'uuid',
-  'objects',
-]
 
 export const rootDirectory = join(__dirname, '..')
 export const sourceDirectory = join(rootDirectory, 'packages')
-const tsconfigFileName = 'tsconfig.json'
-const tsconfigFilePath = join(sourceDirectory, tsconfigFileName)
-const tsconfigContents = readFileSync(tsconfigFilePath).toString()
-const { config } = parseConfigFileTextToJson(tsconfigFileName, tsconfigContents)
-const { options, errors } = convertCompilerOptionsFromJson(
-  config.compilerOptions,
-  rootDirectory,
-  tsconfigFileName,
-)
+export const PACKAGES = readdirSync(sourceDirectory).filter(x => {
+  const pkg = join(sourceDirectory, x)
 
-if (errors && errors.length > 0) {
-  reportDiagnostics(errors)
-
-  process.exit(1)
-}
-
-export const compilerOptions = options
-
-export function reportDiagnostics(diagnostics: Diagnostic[]) {
-  diagnostics.forEach((diagnostic: Diagnostic) => {
-    const message = flattenDiagnosticMessageText(diagnostic.messageText, '\n')
-
-    console.log(`${getFileInfo(diagnostic)} ${message}`)
-  })
-}
-
-export function getFileInfo(diagnostic: Diagnostic) {
-  if (diagnostic.file && diagnostic.start) {
-    const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start)
-
-    return `${diagnostic.file.fileName} (${line + 1},${character + 1}): `
-  }
-
-  return ''
-}
+  return statSync(pkg).isDirectory() && existsSync(join(pkg, 'package.json'))
+})
