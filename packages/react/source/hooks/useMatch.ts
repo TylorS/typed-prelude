f2959b16cb2ab1b763ba988902554391b15001db
Tylor Steinberger <tylor@tryfynd.com>
2019-06-13 19:24:26 -0400

    feat: better types

--- packages/react/source/hooks/useMatch.ts
+++ packages/react/source/hooks/useMatch.ts
@@ -1,19 +1,13 @@
 import { execPure } from '@typed/env'
 import { Match } from '@typed/logic'
-import { Maybe, Nothing } from '@typed/maybe'
-import { isNullOrUndefined } from 'util'
+import { Maybe } from '@typed/maybe'
 import { useDisposable } from './useDisposable'
 import { usePureState } from './usePureState'
 
-export function useMatch<A, B>(match: Match<A, B>, value: A | undefined | null): Maybe<B> {
-  const [matchedValue, setMatchedValue] = usePureState(
-    isNullOrUndefined(value) ? Nothing : match(value),
-  )
+export function useMatch<A, B>(match: Match<A, B>, value: A): Maybe<B> {
+  const [matchedValue, setMatchedValue] = usePureState(() => match(value))
 
-  useDisposable(
-    () => execPure(setMatchedValue(() => (isNullOrUndefined(value) ? Nothing : match(value)))),
-    [value],
-  )
+  useDisposable(() => execPure(setMatchedValue(() => match(value))), [value])
 
   return matchedValue
 }
--- packages/react/source/hooks/useMaybe.ts
+++ packages/react/source/hooks/useMaybe.ts
@@ -1,11 +1,20 @@
 import { Pure } from '@typed/env'
+import { always, pipe } from '@typed/lambda'
 import { Just, Maybe, Nothing } from '@typed/maybe'
 import { usePureState } from './usePureState'
 
 export function useMaybe<A>(defaultValue?: Maybe<A>): UseMaybe<A> {
   const [state, setState] = usePureState(defaultValue || Nothing)
 
-  return [state, (value: A) => setState(() => Just.of(value)), setState(() => Nothing)]
+  return [
+    state,
+    pipe(
+      Just.of,
+      always,
+      setState,
+    ),
+    setState(always(Nothing)),
+  ]
 }
 
 export type UseMaybe<A> = [Maybe<A>, (value: A) => Pure<Maybe<A>>, Pure<Nothing>]
