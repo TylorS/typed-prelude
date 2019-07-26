5134a57e462f96298353412b1923ab7863da0136
Tylor Steinberger <tylor@tryfynd.com>
2019-07-26 19:45:22 -0400

    feat: implement useEffect

---
+++ packages/hooks/source/useEffect.test.ts
@@ -0,0 +1,35 @@
+import { Disposable } from '@typed/disposable'
+import { describe, given, it } from '@typed/test'
+import { createVirtualTimer } from '@typed/timer'
+import { useEffect } from './useEffect'
+import { withHooks } from './withHooks'
+
+export const test = describe(`useEffect`, [
+  given(`() => Disposable`, [
+    it(`executes the function and cleans up using disposable`, ({ equal }) => {
+      let called = 0
+      const delayMs = 1
+      const timer = createVirtualTimer()
+      const fn = withHooks(() =>
+        useEffect(
+          () => ({
+            dispose: () => {
+              called++
+            },
+          }),
+          { timer, delayMs },
+        ),
+      )
+
+      fn().dispose()
+      equal(0, called)
+
+      const disposable = fn()
+
+      timer.timePast(delayMs)
+
+      disposable.dispose()
+      equal(1, called)
+    }),
+  ]),
+])
