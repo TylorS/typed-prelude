5924d6c3473b177b61c09fc6a4f99140b8e6fc57
Tylor Steinberger <tylor@tryfynd.com>
2019-11-17 18:57:39 -0500

    feat: use path-to-regexp for robustness :)

--- packages/routing/source/createRoute.ts
+++ packages/routing/source/createRoute.ts
@@ -1,68 +1,29 @@
 import { Path, pathJoin } from '@typed/history'
 import { Maybe, Nothing } from '@typed/maybe'
-import { Param, pathParts, pathToRegex } from './pathToRegex'
+import { compile, match } from 'path-to-regexp'
 import { Route } from './types'
+
+export type RouteOptions = {
+  readonly encode?: (str: string) => string
+  readonly decode?: (str: string) => string
+}
 
 export function createRoute<A extends Record<string, string | number> = {}>(
   path: string,
+  { encode = encodeURIComponent, decode = decodeURIComponent }: RouteOptions = {},
 ): Route<A> {
-  const { regex, params } = pathToRegex(path)
-  const parts = pathParts(path)
+  const getPath = compile<A>(path, { encode })
+  const getMatch = match<A>(path, { decode })
 
   return {
     path: pathJoin(['/', path]),
     match: (href: Path): Maybe<A> => {
-      const matches = regex.exec(href)
+      const match = getMatch(href)
 
-      return matches ? Maybe.of(findParams<A>(params, {}, matches)) : Nothing
+      return !match ? Nothing : Maybe.of(match.params)
     },
     createPath: (parameters: A, trailingSlash: boolean = false): Maybe<Path> => {
-      if (params.length === 0) {
-        return Maybe.of(pathJoin(parts, trailingSlash))
-      }
-
-      const usableParts = parts.slice()
-
-      for (const { pattern, part, required, name } of params) {
-        const value = parameters[name]
-        const stringValue = value + ''
-
-        if (value && pattern.test(stringValue)) {
-          usableParts[part] = stringValue
-        } else if (required) {
-          return Nothing
-        } else {
-          usableParts[part] = ''
-        }
-      }
-
-      return Maybe.of(pathJoin(usableParts, trailingSlash))
+      return Maybe.of(pathJoin([getPath(parameters)], trailingSlash))
     },
-  }
-}
-
-function findParams<A>(
-  params: Param[],
-  parameters: Record<string, any>,
-  matches: RegExpExecArray,
-): A {
-  for (let i = 0; i < params.length; ++i) {
-    const { name, pattern } = params[i]
-
-    const paramMatches = pattern.exec(matches[i + 1])
-
-    if (paramMatches) {
-      parameters[name] = parseParam(paramMatches[0])
-    }
-  }
-
-  return parameters as A
-}
-
-function parseParam(param: any): any {
-  try {
-    return JSON.parse(param)
-  } catch {
-    return param
   }
 }
