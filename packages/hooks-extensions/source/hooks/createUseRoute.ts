25d094581ba76267b1963d914438c058e22959cc
Tylor Steinberger <tylor@tryfynd.com>
2019-11-17 20:09:04 -0500

    chore: optionally allow withRoute

--- packages/hooks-extensions/source/hooks/createUseRoute.ts
+++ packages/hooks-extensions/source/hooks/createUseRoute.ts
@@ -1,17 +1,17 @@
 import { CreateHookContext, withCreateHook } from '@typed/hooks'
-import { Arity1 } from '@typed/lambda'
+import { Arity1, id } from '@typed/lambda'
 import { Path, Route } from '@typed/routing'
 import { createUseHistoryEnv } from '../channels'
 import { createUseMatch } from './createUseMatch'
 
-export const createUseRoute = <A, B>(
+export const createUseRoute = <A, B, C = B>(
   context: CreateHookContext,
-  route: Route<A>,
-  withRoute: Arity1<A, B>,
+  route: Route<A, B>,
+  withRoute: Arity1<B, C> = id as Arity1<B, C>,
 ) => {
   const createUseRouteHook = withCreateHook(
     createHook => [createHook(createUseHistoryEnv), createHook(createUseMatch)] as const,
-    ([useHistory, useMatch], route: Route<A>, withRoute: Arity1<A, B>) => {
+    ([useHistory, useMatch], route: Route<A, B>, withRoute: Arity1<B, C> = id as Arity1<B, C>) => {
       const { location } = useHistory()
       const matched = useMatch(location.pathname as Path, route.match, withRoute)
 
