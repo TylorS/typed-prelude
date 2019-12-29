7d3ee9d9a68254b7b226086779d0fb3e9299f3d6
Tylor Steinberger <tylor@tryfynd.com>
2019-12-29 12:16:10 -0500

    fix: unneeded param

--- packages/jwt/source/isExpired.ts
+++ packages/jwt/source/isExpired.ts
@@ -1,8 +1,8 @@
 import { getExpiration } from './getExpiration'
 import { Jwt } from './Jwt'
 
-export function isExpired(jwt: Jwt, expirationKey: string = 'exp'): boolean {
-  const expiration = getExpiration(jwt, expirationKey)
+export function isExpired(jwt: Jwt): boolean {
+  const expiration = getExpiration(jwt)
   const now = new Date()
 
   return now >= expiration
