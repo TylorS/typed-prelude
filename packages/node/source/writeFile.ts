f8dd4f2ebd9586ba1bb751f92b17a080e0b625d3
Tylor Steinberger <tylor@tryfynd.com>
2020-01-14 15:44:37 -0500

    fix: type error with rollup

--- packages/node/source/writeFile.ts
+++ packages/node/source/writeFile.ts
@@ -1,9 +1,14 @@
 import { Disposable } from '@typed/disposable'
 import { Effect } from '@typed/effects'
+import { Either } from '@typed/either'
+import { Env } from '@typed/env'
 import { Future } from '@typed/future'
 import * as fs from 'fs'
 
-export function* writeFile(filePath: string, contents: string | Buffer) {
+export function* writeFile(
+  filePath: string,
+  contents: string | Buffer,
+): Effect<Env<never, Either<Error, void>>, Either<Error, void>, any> {
   return yield* Effect.fromEnv(
     Future.create<never, Error, void>((reject, resolve) => {
       const disposable = Disposable.lazy()
