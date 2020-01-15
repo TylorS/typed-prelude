60df4678d24908d093c821f8867b477f36d8b19b
Tylor Steinberger <tylor@tryfynd.com>
2020-01-15 18:05:24 -0500

    feat: orElse

---
+++ packages/either/source/orElse.ts
@@ -0,0 +1,16 @@
+import { curry } from '@typed/lambda'
+import { Either } from './Either'
+import { unpack } from './unpack'
+
+export const orElse = curry(__orElse) as {
+  <A, B>(value: A, validation: Either<B, A>): A
+  <A>(value: A): <B>(validation: Either<B, A>) => A
+}
+
+function __orElse<A, B>(value: A, validation: Either<B, A>): A {
+  return unpack(
+    () => value,
+    b => b,
+    validation,
+  )
+}
