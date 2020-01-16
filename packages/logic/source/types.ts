5c04bbe9fc7298bbd1366ab565af0643d4e8418e
Tylor Steinberger <tylor@tryfynd.com>
2020-01-16 08:03:24 -0500

    feat: curry Match.map

--- packages/logic/source/types.ts
+++ packages/logic/source/types.ts
@@ -1,12 +1,16 @@
-import { Arity1, Is, Predicate } from '@typed/lambda'
+import { Arity1, curry, Is, Predicate } from '@typed/lambda'
 import { map as maybeMap, Maybe, Nothing } from '@typed/maybe'
 
 export interface Match<A, B> extends Arity1<A, Maybe<B>> {}
 
 export namespace Match {
-  export const map = <A, B, C>(fn: (value: B) => C, match: Match<A, B>): Match<A, C> => (
-    value: A,
-  ) => maybeMap(fn, match(value))
+  export const map = curry(
+    <A, B, C>(fn: (value: B) => C, match: Match<A, B>): Match<A, C> => (value: A) =>
+      maybeMap(fn, match(value)),
+  ) as {
+    <A, B, C>(fn: (value: B) => C, match: Match<A, B>): Match<A, C>
+    <B, C>(fn: (value: B) => C): <A>(match: Match<A, B>) => Match<A, C>
+  }
 
   export function fromPredicate<A>(predicate: Predicate<A>): Match<A, A>
   export function fromPredicate<A, B>(predicate: Is<B>): Match<A, B>
