aec77699f4ce674963e14e7811e9ed76576e6c0f
Tylor Steinberger <tylor@tryfynd.com>
2020-01-18 13:45:47 -0500

    fix: update dispatch anytime reducer changes

--- packages/hooks/source/useReducer.ts
+++ packages/hooks/source/useReducer.ts
@@ -5,8 +5,8 @@ import { useState } from './useState'
 export function* useReducer<A, B>(reducer: Arity2<A, B, A>, seed: A) {
   const [state, updateState] = yield* useState(seed)
   const dispatch = yield* useMemo(
-    () => (event: B) => updateState(state => reducer(state, event)),
-    [],
+    reducer => (event: B) => updateState(state => reducer(state, event)),
+    [reducer],
   )
 
   return [state, dispatch] as const
