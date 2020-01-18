9426358dc3c1b262ba0feedfcca48369dc60a7bc
Tylor Steinberger <tylor@tryfynd.com>
2020-01-17 23:10:56 -0500

    fix: memoize returned function

--- packages/hooks/source/useCallback.ts
+++ packages/hooks/source/useCallback.ts
@@ -1,6 +1,6 @@
-import { Arity1 } from '@typed/lambda'
+import { Arity1, memoize } from '@typed/lambda'
 import { useMemo } from './useMemo'
 
 export function* useCallback<A, B>(fn: Arity1<A, B>, deps: ReadonlyArray<any>) {
-  return yield* useMemo(_ => fn, deps)
+  return yield* useMemo(_ => memoize(fn), deps)
 }
