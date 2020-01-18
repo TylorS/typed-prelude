99198cedc810db90c37a224ea8efeb556d659cf0
Tylor Steinberger <tylor@tryfynd.com>
2020-01-18 13:48:24 -0500

    feat: useMapChannel and useCombineChannels

--- packages/hooks/source/useChannel.ts
+++ packages/hooks/source/useChannel.ts
@@ -1,9 +1,27 @@
 import { get } from '@typed/effects'
-import { Channel } from './Channel'
+import { Arity1 } from '@typed/lambda'
+import { Channel, ChannelValue } from './Channel'
 import { HookEnvironment } from './HookEnvironment'
+import { useMemo } from './useMemo'
 
 export function* useChannel<A>(channel: Channel<A>) {
   const { useChannel } = yield* get<HookEnvironment>()
 
   return yield* useChannel(channel)
+}
+
+export function* useMapChannel<A, B>(channel: Channel<A>, fn: Arity1<A, B>) {
+  const value = yield* useChannel(channel)
+
+  return yield* useMemo(fn, [value])
+}
+
+export function* useCombineChannels<A extends ReadonlyArray<Channel<any>>>(...channels: A) {
+  const combined: Array<ChannelValue<A[keyof A]>> = [] as any
+
+  for (let i = 0; i < channels.length; ++i) {
+    combined[i] = yield* useChannel(channels[i])
+  }
+
+  return (combined as any) as { readonly [K in keyof A]: ChannelValue<A[K]> }
 }
