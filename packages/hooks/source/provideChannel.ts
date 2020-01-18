04c436abaf53c27dce71921de90c3ecf61c2d128
Tylor Steinberger <tylor@tryfynd.com>
2020-01-18 13:11:14 -0500

    refactor: create UpdateChannel type alias to remove duplication

--- packages/hooks/source/provideChannel.ts
+++ packages/hooks/source/provideChannel.ts
@@ -5,11 +5,11 @@ import { Channel } from './Channel'
 import { HookEnvironment, InitialState } from './HookEnvironment'
 import { useDepChange } from './useDepChange'
 
+type UpdateChannel<A> = (value: A) => Effect<Env<never, any>, A, any>
+
 export function* provideChannel<A>(channel: Channel<A>, initial: InitialState<A>) {
   const { useChannel, provideChannel, useRef } = yield* get<HookEnvironment>()
-  const [updateChannel, setUpdateChannel] = yield* useRef<
-    (value: A) => Effect<Env<never, any>, A, any>
-  >()
+  const [updateChannel, setUpdateChannel] = yield* useRef<UpdateChannel<A>>()
   const channelHasChanged = yield* useDepChange(channel)
   const currentValue = yield* useChannel(channel)
 
@@ -17,8 +17,5 @@ export function* provideChannel<A>(channel: Channel<A>, initial: InitialState<A>
     setUpdateChannel(yield* provideChannel(channel, initial))
   }
 
-  return [
-    currentValue,
-    fromJust(updateChannel.current as Just<(value: A) => Effect<Env<never, any>, A, any>>),
-  ] as const
+  return [currentValue, fromJust(updateChannel.current as Just<UpdateChannel<A>>)] as const
 }
