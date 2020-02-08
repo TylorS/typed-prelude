dc9075d0bb245bb42f0760de04c141ee17206d2f
Tylor Steinberger <tylor@tryfynd.com>
2020-02-08 11:06:27 -0500

    chore: separate createServerDom

--- packages/dom/source/index.ts
+++ packages/dom/source/index.ts
@@ -2,6 +2,7 @@ export * from './addEventListener'
 export * from './checks'
 export * from './createServerDomEnv'
 export * from './createDomEnv'
+export * from './createServerDomEnv'
 export * from './querySelector'
 export * from './types'
 
