ce01622117373d1a3cfff99bb49a7a2b31f6334b
Tylor Steinberger <tylor@tryfynd.com>
2020-02-08 13:57:05 -0500

    feat: add ReturnType to useRemoteData

--- packages/hooks/source/useRemoteData.ts
+++ packages/hooks/source/useRemoteData.ts
@@ -1,9 +1,25 @@
+import { Effects } from '@typed/effects'
+import { Arity1, IO } from '@typed/lambda'
 import { Failure, Loading, NoData, RemoteData, Success } from '@typed/remote-data'
+import { HookEffects } from './HookEffects'
 import { InitialState } from './HookEnvironment'
 import { useMemo } from './useMemo'
 import { useState } from './useState'
 
-export function* useRemoteData<A, B>(initial: InitialState<RemoteData<A, B>> = NoData) {
+export type RemoteDataActions<A, B> = {
+  readonly set: (remoteData: RemoteData<A, B>) => Effects<never, RemoteData<A, B>>
+  readonly update: (
+    updateFn: Arity1<RemoteData<A, B>, RemoteData<A, B>>,
+  ) => Effects<never, RemoteData<A, B>>
+  readonly loading: () => Effects<never, RemoteData<A, B>>
+  readonly clear: () => Effects<never, RemoteData<A, B>>
+  readonly success: (value: B) => Effects<never, RemoteData<A, B>>
+  readonly failure: (value: A) => Effects<never, RemoteData<A, B>>
+}
+
+export function* useRemoteData<A, B>(
+  initial: InitialState<RemoteData<A, B>> = NoData,
+): HookEffects<never, readonly [IO<RemoteData<A, B>>, RemoteDataActions<A, B>]> {
   const [getRemoteData, update] = yield* useState<RemoteData<A, B>>(initial)
   const set = (remoteData: RemoteData<A, B>) => update(() => remoteData)
   const actions = yield* useMemo(
