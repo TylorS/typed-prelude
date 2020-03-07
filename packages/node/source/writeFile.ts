33c41df350b4e14412a6cb8eaa072585ea91da7a
Tylor Steinberger <tylor@simspace.local>
2020-03-07 10:49:38 -0500

    feat: update @typed/node with effect types

--- packages/node/source/writeFile.ts
+++ packages/node/source/writeFile.ts
@@ -1,14 +1,13 @@
 import { Disposable } from '@typed/disposable'
 import { Effect } from '@typed/effects'
 import { Either } from '@typed/either'
-import { Env } from '@typed/env'
 import { Future } from '@typed/future'
 import * as fs from 'fs'
 
 export function* writeFile(
   filePath: string,
   contents: string | Buffer,
-): Effect<Env<never, Either<Error, void>>, Either<Error, void>, any> {
+): Effect<never, Either<Error, void>> {
   return yield* Effect.fromEnv(
     Future.create<never, Error, void>((reject, resolve) => {
       const disposable = Disposable.lazy()
