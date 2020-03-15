3ac5e83aec8364ea4e3dede483bdc3af0400c9aa
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-03-15 09:48:23 -0400

    feat: simplify with Effects

--- packages/hooks/source/useChannel.ts
+++ packages/hooks/source/useChannel.ts
@@ -1,4 +1,4 @@
-import { co, Computation, Env } from '@typed/effects'
+import { co, Effects } from '@typed/effects'
 import { Channel } from './Channel'
 import { getHookEnv } from './getHookEnv'
 import { HookEnv, InitialState, UseChannel } from './HookEnvironment'
@@ -6,10 +6,10 @@ import { HookEnv, InitialState, UseChannel } from './HookEnvironment'
 export const useChannel: <E, A>(
   channel: Channel<E, A>,
   initial?: InitialState<A>,
-) => Computation<Env<HookEnv, any> | Env<E, any>, UseChannel<E, A>, any> = co(function* useChannel<
-  E,
-  A
->(channel: Channel<E, A>, initial?: InitialState<A>) {
+) => Effects<HookEnv & E, UseChannel<E, A>> = co(function* useChannel<E, A>(
+  channel: Channel<E, A>,
+  initial?: InitialState<A>,
+) {
   const { useChannel } = yield* getHookEnv()
 
   return yield* useChannel(channel, initial)
