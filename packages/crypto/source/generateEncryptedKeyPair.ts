1623df9f99bb42cd4d387796257457742a7ccffc
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-03-15 20:24:58 -0400

    refactor: typeo in comment

--- packages/crypto/source/generateEncryptedKeyPair.ts
+++ packages/crypto/source/generateEncryptedKeyPair.ts
@@ -15,7 +15,7 @@ import { EncryptedKeyPair } from './types'
  * be used to encrypt/decrypt all application data. The encrypted views of the CryptoKeyPair can safely be
  * persisted or sent over the internet. Furthermore, if the user chooses to change their passsword
  * one must only re-encrypt the generated CryptoKeyPair with the new AES CryptoKey.
- * @param aesKey {CryptoKey} - A symmetrical key that is allows to encrypt/decrypt
+ * @param aesKey {CryptoKey} - A symmetrical key that is allowed to encrypt/decrypt
  */
 export function* generateEncryptedKeyPair(
   aesKey: CryptoKey,
