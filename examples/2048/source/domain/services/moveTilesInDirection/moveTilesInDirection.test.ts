f3865cc6a0d13b3d52bc236ff52148ef1ce47cd9
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-03-19 23:05:29 -0400

    test: add ids to test

--- examples/2048/source/domain/services/moveTilesInDirection/moveTilesInDirection.test.ts
+++ examples/2048/source/domain/services/moveTilesInDirection/moveTilesInDirection.test.ts
@@ -1,35 +1,43 @@
 import { describe, given, it } from '@typed/test'
+import { NodeGenerator, uuid4 } from '@typed/uuid'
 import { Bounds, Direction, Tiles } from '../../model/Grid'
 import { convertSizeToBounds } from '../convertSizeToBounds'
 import { moveTilesInDirection } from './moveTilesInDirection'
+
+const { randomUuidSeed } = new NodeGenerator()
+const createId = () => ({ id: uuid4(randomUuidSeed()) })
 
 export const test = describe(`moveTilesInDirection`, [
   given(`3x3 Bounds, Direction, and Tiles`, [
     it(`returns Tiles moved up`, ({ equal }) => {
       const bounds: Bounds = convertSizeToBounds([3, 3])
+      const aId = createId()
+      const bId = createId()
+      const cId = createId()
+
       const tiles: Tiles = [
-        { value: 2, position: [0, 0] },
-        { value: 2, position: [0, 1] },
-        { value: 4, position: [2, 2] },
+        { ...aId, value: 2, position: [0, 0] },
+        { ...bId, value: 2, position: [0, 1] },
+        { ...cId, value: 4, position: [2, 2] },
       ]
       const expectedUp: Tiles = [
-        { value: 4, position: [0, 0] },
-        { value: 4, position: [2, 0] },
+        { ...bId, value: 4, position: [0, 0] },
+        { ...cId, value: 4, position: [2, 0] },
       ]
       const expectedDown: Tiles = [
-        { value: 4, position: [2, 2] },
-        { value: 4, position: [0, 2] },
+        { ...cId, value: 4, position: [2, 2] },
+        { ...aId, value: 4, position: [0, 2] },
       ]
       const expectedLeft: Tiles = [
-        { value: 2, position: [0, 0] },
-        { value: 2, position: [0, 1] },
-        { value: 4, position: [0, 2] },
+        { ...aId, value: 2, position: [0, 0] },
+        { ...bId, value: 2, position: [0, 1] },
+        { ...cId, value: 4, position: [0, 2] },
       ]
 
       const expectedRight: Tiles = [
-        { value: 4, position: [2, 2] },
-        { value: 2, position: [2, 1] },
-        { value: 2, position: [2, 0] },
+        { ...cId, value: 4, position: [2, 2] },
+        { ...bId, value: 2, position: [2, 1] },
+        { ...aId, value: 2, position: [2, 0] },
       ]
 
       equal(expectedUp, moveTilesInDirection(bounds, Direction.Up, tiles))
