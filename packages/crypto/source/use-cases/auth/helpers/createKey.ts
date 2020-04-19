c5b8f6b77e3e964c682182524db780ecbfc37825
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-04-19 16:45:32 -0400

    feat: remove use-cases from crypto
    These should probably stay inside of applications

--- packages/crypto/source/use-cases/auth/helpers/createKey.ts
+++
@@ -1,3 +0,0 @@
-import { KEY_SEPARATOR } from './constants'
-
-export const createKey = (prefix: string, name: string) => `${prefix}${KEY_SEPARATOR}${name}`
--- packages/crypto/source/use-cases/auth/helpers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './getAvailableSalts'
-export * from './getSavedEncryptedKeyPair'
-export * from './orCryptoFailure'
-export * from './saveEncryptedKeyPair'
