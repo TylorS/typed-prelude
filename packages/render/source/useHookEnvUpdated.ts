c07a7505da80462d8a3c10ff126b6078fb73e2a2
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-05-09 16:32:33 -0400

    feat: move rendering logic to useKeyManager

--- packages/render/source/useHookEnvUpdated.ts
+++ packages/render/source/useHookEnvUpdated.ts
@@ -1,40 +1,12 @@
-import { Disposable, onDisposed } from '@typed/disposable'
+import { Disposable } from '@typed/disposable'
 import { get } from '@typed/effects'
-import {
-  HookEnvironment,
-  HookEnvironmentEventType,
-  HooksManagerEnv,
-  InitialState,
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-} from '@typed/hooks'
-import { withDefault } from '@typed/maybe/source'
+import { HookEnvironment, HookEnvironmentEventType, HooksManagerEnv, useMemo } from '@typed/hooks'
 
 /**
  * Listen for updated events regarding a particular hook environment.
  */
 export function* useHookEnvUpdated(env: HookEnvironment, onUpdated: () => Disposable) {
-  const [currentlyUpdating, setCurrentlyUpdated] = yield* useRef(InitialState.of(false))
-  const [shouldBeUpdated, setShouldBeUpdated] = yield* useRef(InitialState.of(false))
   const { hooksManager } = yield* get<HooksManagerEnv>()
-  const isUpdating = yield* useCallback(() => withDefault(false, currentlyUpdating.current), [
-    currentlyUpdating.current,
-  ])
-
-  yield* useEffect(
-    (_) => {
-      const shouldRunEffect = withDefault(false, shouldBeUpdated.current)
-
-      if (!shouldRunEffect) {
-        return Disposable.None
-      }
-
-      return onDisposed(() => setCurrentlyUpdated(false), onUpdated())
-    },
-    [shouldBeUpdated],
-  )
 
   return yield* useMemo(
     (_) =>
@@ -45,23 +17,8 @@ export function* useHookEnvUpdated(env: HookEnvironment, onUpdated: () => Dispos
             const isUpdatedEnvironment = env.id === hookEnvironment.id && updated
             const itWouldBeFasterToLetAParentRender = hooksManager.hasUpdatedParents(env)
 
-            if (
-              isUpdatedEnvironment &&
-              !itWouldBeFasterToLetAParentRender &&
-              !isUpdating(hookEnvironment)
-            ) {
-              // Currently built to only allow for 1 update to be occurring at a time
-              // Since a render _should_ z
-
-              const disposable = onUpdated()
-
-              hookEnvironment.addDisposable(disposable)
-
-              return onDisposed(() => setCurrentlyUpdated(false), disposable)
-            }
-
             if (isUpdatedEnvironment && !itWouldBeFasterToLetAParentRender) {
-              setShouldBeUpdated(true)
+              return onUpdated()
             }
 
             return Disposable.None
--- packages/render/source/useKeyManager.ts
+++ packages/render/source/useKeyManager.ts
@@ -5,12 +5,13 @@ import {
   getEnvironmentByKey,
   HookEffects,
   HookEnv,
+  InitialState,
   runWithHooks,
   useMemo,
   useRef,
   UseRef,
 } from '@typed/hooks'
-import { fromJust, isJust, isNothing, Just } from '@typed/maybe'
+import { fromJust, isJust, isNothing, Just, withDefault } from '@typed/maybe'
 import { patch, PatchEnv } from './Patch'
 import { useHookEnvUpdated } from './useHookEnvUpdated'
 
@@ -24,6 +25,16 @@ export function* useKeyManager<E, B, C>(
 ): ChannelEffects<HookEnv & TimerEnv & PatchEnv<C, B> & E, B> {
   const env = yield* get()
   const hookEnvironment = yield* getEnvironmentByKey(key)
+  const [currentlyUpdating, setCurrentlyUpdating] = yield* useRef(InitialState.of(false))
+  const [shouldBeUpdated, setShouldBeUpdated] = yield* useRef(InitialState.of(false))
+  const checkIsUpdating = yield* useMemo(
+    () => () => withDefault(false, currentlyUpdating.current),
+    [],
+  )
+  const checkShouldBeUpdated = yield* useMemo(
+    () => () => withDefault(false, shouldBeUpdated.current),
+    [],
+  )
   const [renderable, setRenderable] = yield* useRef<unknown, B>()
   const isFirstRun = isNothing(renderable.current)
   const renderedRef = yield* useRef<unknown, C>()
@@ -31,12 +42,22 @@ export function* useKeyManager<E, B, C>(
   const applyUpdate = yield* useMemo(
     (_) =>
       // Allows for an effect to re-render itself
-      function* (): ChannelEffects<HookEnv & TimerEnv & E & PatchEnv<C, B>, void> {
-        if (isJust(rendered.current)) {
+      function* runPatch(): ChannelEffects<HookEnv & TimerEnv & E & PatchEnv<C, B>, void> {
+        if (isJust(rendered.current) && !checkIsUpdating()) {
+          setCurrentlyUpdating(true)
           const updated = yield* runWithHooks(render(renderedRef), hookEnvironment)
 
           setRenderable(updated)
           setRendered(yield* patch(fromJust(rendered.current), updated))
+          setCurrentlyUpdating(false)
+
+          if (checkShouldBeUpdated()) {
+            setShouldBeUpdated(false)
+
+            return yield* runPatch()
+          }
+        } else {
+          setShouldBeUpdated(true)
         }
       },
     [rendered, hookEnvironment],
