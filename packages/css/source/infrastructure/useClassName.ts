680d73704af83fb00906327e55c6840a3566232f
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-05-12 18:37:40 -0400

    refactor: improve how useClassName functions

--- packages/css/source/infrastructure/useClassName.ts
+++ packages/css/source/infrastructure/useClassName.ts
@@ -1,14 +1,22 @@
 import {
   arrayBufferToString,
+  CryptoEffects,
   CryptoEnv,
   CryptoFailure,
   generateShaHash,
   stringToArrayBuffer,
 } from '@typed/crypto'
 import { Effects, get, sequence } from '@typed/effects'
-import { HookEnv, useMemo, useMemoEffect } from '@typed/hooks'
+import { HookEnv, useMemoEffect } from '@typed/hooks'
 import { isNotUndefined, isNumber } from '@typed/logic'
-import { ClassName, Css, CssProperties, GenerateClassName, NestedCssProperties } from '../model'
+import {
+  ClassName,
+  Css,
+  CssProperties,
+  GenerateClassName,
+  NestedCssProperties,
+  Rule,
+} from '../model'
 import { classNames } from './classNames'
 import { CssEnv } from './CssEnv'
 import { getCss } from './getCss'
@@ -26,73 +34,73 @@ const toPx = (sOrN: string | number) => (isNumber(sOrN) ? `${sOrN}px` : sOrN)
 const hyphenate = (s: string) => s.replace(HYPHENATE_REGEX, HYPHENATE_REPLACEMENT).toLowerCase()
 const notAnd = (s: string) => s.replace(AND_REGEX, '')
 
-export const useClassName: GenerateClassName<
-  CssEnv & HookEnv & CryptoEnv & CryptoFailure
-> = function* (...properties) {
-  const hashes = yield* useMemoEffect(
-    (...props) => sequence(generatePropertyHashes, props),
-    properties,
-  )
-  const className = yield* useMemo((hs) => classNames(...hs.flat().sort()), [hashes])
+export const useClassName: GenerateClassName<CssEnv & HookEnv & CryptoEnv & CryptoFailure> = (
+  ...properties
+) =>
+  useMemoEffect(function* (...props) {
+    const { rules, styleSheet } = yield* get()
 
-  return className
-}
+    const startSize = rules.size
+    const generateClassNames = yield* sequence(generatePropertyClassNames, props)
 
-function* generatePropertyHashes(
+    if (styleSheet && startSize !== rules.size) {
+      styleSheet.textContent = getCss(rules)
+    }
+
+    return classNames(...generateClassNames.flat().sort())
+  }, properties)
+
+function* generatePropertyClassNames(
   properties: NestedCssProperties,
+  classNames: readonly ClassName[] = [],
   nestedSelector: string = '',
   media: string = '',
-): Effects<CssEnv & HookEnv & CryptoEnv & CryptoFailure, ClassName[]> {
+): Effects<CssEnv & HookEnv & CryptoEnv & CryptoFailure, readonly ClassName[]> {
   const {
-    styleSheet,
     rules,
     classNamePrefix = DEFAULT_CLASS_NAME_PREFIX,
     classNameLength = DEFAULT_CLASS_NAME_LENGTH,
   } = yield* get<CssEnv>()
-  const startSize = rules.size
-  const classNames: ClassName[] = []
-  const keys = Object.keys(properties).sort() as (keyof NestedCssProperties)[]
 
-  for (const key of keys) {
-    if (key !== '$nest') {
-      const props = getPropertiesString(properties, key)
+  const keys = Object.keys(properties)
+    .sort()
+    .filter((x) => x !== '$nest') as (keyof CssProperties)[]
 
-      yield* updateRule({
-        ruleKey: `${media}${nestedSelector}${props}`,
-        props,
-        nestedSelector,
-        media,
-        rules,
-        classNames,
-        classNamePrefix,
-        classNameLength,
-      })
-    }
+  const propertyClassNames = yield* sequence(function* (key) {
+    const props = getPropertiesString(properties, key)
+
+    return yield* getClassName({
+      ruleKey: `${media}${nestedSelector}${props}`,
+      props,
+      nestedSelector,
+      media,
+      rules,
+      classNamePrefix,
+      classNameLength,
+    })
+  }, keys)
+
+  if (!properties.$nest) {
+    return [...classNames, ...propertyClassNames]
   }
 
-  if (properties.$nest) {
-    const nestedKeys = Object.keys(properties.$nest).sort()
+  const nestedProperties = properties.$nest
+  const nestedKeys = Object.keys(properties.$nest).sort()
+  const nestedClassNames = yield* sequence(function* (nestedKey) {
+    const isMediaQuery = nestedKey.startsWith('@')
 
-    for (const nestedKey of nestedKeys) {
-      const isMediaQuery = nestedKey.startsWith('@')
-      const nestedClassNames = yield* generatePropertyHashes(
-        properties.$nest[nestedKey]!,
-        notAnd(isMediaQuery ? nestedSelector : nestedSelector + ' ' + notAnd(nestedKey)).trim(),
-        isMediaQuery ? nestedKey : media,
-      )
+    return yield* generatePropertyClassNames(
+      nestedProperties[nestedKey]!,
+      [],
+      notAnd(isMediaQuery ? nestedSelector : nestedSelector + ' ' + notAnd(nestedKey)).trim(),
+      isMediaQuery ? nestedKey : media,
+    )
+  }, nestedKeys)
 
-      classNames.push(...nestedClassNames)
-    }
-  }
-
-  if (styleSheet && startSize !== rules.size) {
-    styleSheet.textContent = getCss(rules)
-  }
-
-  return classNames
+  return [...classNames, ...propertyClassNames, ...nestedClassNames.flat()]
 }
 
-type UpdateRuleOptions = {
+type GetClassNameOptions = {
   // Used to index in rules map
   ruleKey: string
   // properties being wrapped into a atomic class
@@ -103,32 +111,27 @@ type UpdateRuleOptions = {
   media: string
   // Map of all rules currently in use
   rules: Map<string, readonly [ClassName, Css]>
-  // List of classNames generated
-  classNames: ClassName[]
   // Prefix used to ensure a valid className
   classNamePrefix: string
   // Length of className hash - convenient if there's ever a collision
   classNameLength: number
 }
 
-function* updateRule(options: UpdateRuleOptions) {
-  const { ruleKey, rules, classNames } = options
+/**
+ * Get the className for an existing rule or generate it
+ */
+function* getClassName(options: GetClassNameOptions) {
+  const { ruleKey, rules } = options
+  const [className] = rules.has(ruleKey) ? rules.get(ruleKey)! : yield* getRule(options)
 
-  if (rules.has(ruleKey)) {
-    const [className] = rules.get(ruleKey)!
-
-    classNames.push(className)
-
-    return
-  }
-
-  const rule = yield* getRule(options)
-
-  rules.set(ruleKey, rule)
-  classNames.push(rule[0])
+  return className
 }
 
-function getPropertiesString(properties: CssProperties, key: keyof CssProperties) {
+/**
+ * Deterministically generates a CSS string representation of a CSS property with any of it's
+ * possible fallback values.
+ */
+function getPropertiesString(properties: CssProperties, key: keyof CssProperties): string {
   const hyphenatedKey = hyphenate(key)
   const values = toArray(properties[key]).filter(isNotUndefined).reverse()
   const css = values.reduce(
@@ -139,20 +142,25 @@ function getPropertiesString(properties: CssProperties, key: keyof CssProperties
   return `{${css.join(`;`)}}`
 }
 
-function* getRule(options: UpdateRuleOptions) {
-  const { ruleKey, props, media, nestedSelector, classNamePrefix, classNameLength } = options
-  const className = yield* getClassName(ruleKey, classNamePrefix, classNameLength)
+/**
+ * Generate a CSS rule
+ */
+function* getRule(options: GetClassNameOptions): CryptoEffects<unknown, Rule> {
+  const { ruleKey, props, media, nestedSelector, classNamePrefix, classNameLength, rules } = options
+  const className = yield* generateClassName(ruleKey, classNamePrefix, classNameLength)
   const css = `.${className}${nestedSelector}${props}` as Css
+  const rule: Rule = media ? [className, `${media}{${css}}` as Css] : [className, css]
 
-  if (media) {
-    return [className, `${media}{${css}}` as Css] as const
-  }
+  rules.set(ruleKey, rule)
 
-  return [className, css] as const
+  return rule
 }
 
-function* getClassName(hashable: string, classNamePrefix: string, classNameLength: number) {
-  const buffer = stringToArrayBuffer(hashable)
+/**
+ * Generate a SHA-1 of the ruleKey to produce a className
+ */
+function* generateClassName(ruleKey: string, classNamePrefix: string, classNameLength: number) {
+  const buffer = stringToArrayBuffer(ruleKey)
   const hash = yield* generateShaHash(1, buffer)
   const className = escape(
     classNamePrefix + convertToHex(arrayBufferToString(hash)).slice(0, classNameLength),
@@ -172,6 +180,9 @@ function escape(str: string): ClassName {
   return str.replace(CLASS_NAME_ESCAPE_REGEX, CLASS_NAME_ESCAPE_REPLACEMENT) as ClassName
 }
 
+/**
+ * Ensure a hexadecimal
+ */
 function convertToHex(str: string) {
   let hex = ''
   for (let i = 0; i < str.length; i++) {
