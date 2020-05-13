8657c32a0cfb8ea5c006bc2e3625c75cc9340670
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-05-13 10:13:04 -0400

    fix: ensure HTMLStyleElement is accepted

--- packages/css/source/infrastructure/CssEnv.ts
+++ packages/css/source/infrastructure/CssEnv.ts
@@ -1,7 +1,7 @@
 import { Rules } from '../model'
 
 export type CssEnv = {
-  readonly styleSheet: { textContent: string }
+  readonly styleSheet: { textContent: string | null }
   readonly rules: Rules
   readonly classNamePrefix?: string
   readonly classNameLength?: number
