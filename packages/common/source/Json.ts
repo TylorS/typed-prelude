730883a768ebc80182c4c63983309634b8673cc3
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-05-21 11:10:04 -0400

    feat: start new json validation package

--- packages/common/source/Json.ts
+++ packages/common/source/Json.ts
@@ -1,7 +1,5 @@
 export type Json = JsonPrimitive | JsonArray | JsonObject
 
 export type JsonPrimitive = string | number | boolean | null
-
-export interface JsonArray extends Array<Json> {}
-
-export interface JsonObject extends Record<string, Json> {}
+export type JsonArray = readonly Json[]
+export type JsonObject = { readonly [key: string]: Json }
