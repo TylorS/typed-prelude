29284fbb056412e3632a9db241de0bb622f29136
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-05-30 22:29:18 -0400

    feat: add some missing exports + checkHttpRequest

--- packages/io/source/index.ts
+++ packages/io/source/index.ts
@@ -1,1 +1,13 @@
+export { Guard, guard } from './guard/Guard'
+export {
+  Decoder,
+  DecodeEffect,
+  DecodeFailure,
+  DecodeError,
+  decodeFailure,
+  catchDecodeFailure,
+} from './decoder'
+export { Encoder } from './encoder'
+
 export * from './types'
+export * from './checkHttpRequest'
