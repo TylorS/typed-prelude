fb2c3f6a18eed9e16fe23f894f57679c225026d3
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-06-08 18:29:19 -0400

    feat: ensure not a breaking change

--- packages/io/source/types/Type.ts
+++ packages/io/source/types/Type.ts
@@ -25,5 +25,14 @@ export namespace Type {
   })
 }
 
+export type Mixed = Type<any, any>
+
 export interface Any extends Type<any, any> {}
 export const Any: Any = Type.fromGuard({ is: (_): _ is any => true }, `Any`, `any`)
+
+export interface Unknown extends Type<unknown, unknown> {}
+export const Unknown: Unknown = Type.fromGuard(
+  { is: (_): _ is unknown => true },
+  `Unknown`,
+  `unknown`,
+)
