5f1a69f57c72bb04bebaff4af5b487a46975f872
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-06-08 17:50:46 -0400

    feat: array add memberType

--- packages/io/source/types/Array.ts
+++ packages/io/source/types/Array.ts
@@ -1,22 +1,22 @@
-import * as G from '../guard'
 import { refinement } from './refinement'
 import { Mixed, Type } from './Type'
 
-export const Array: ArrayType<unknown> = Type.fromGuard(
-  G.Array,
-  `UnknownArray`,
-  `ReadonlyArray<unknown>`,
-)
+export const Array: ArrayType<Mixed> = array(Mixed, `ReadonlyArray<unknown>`)
 
-export type ArrayType<A> = Type<ReadonlyArray<A>>
+export interface ArrayType<A extends Type> extends Type<ReadonlyArray<Type.Of<A>>> {
+  readonly memberType: A
+}
 
 export function array<A extends Mixed>(
   type: A,
   name = `ReadonlyArray<${type.name}>`,
-): ArrayType<Type.Of<A>> {
-  return refinement(
-    Array,
-    (as): as is ReadonlyArray<Type.Of<A>> => as.every((a) => type.is(a)),
-    name,
-  )
+): ArrayType<A> {
+  return {
+    ...refinement(
+      Array,
+      (as): as is ReadonlyArray<Type.Of<A>> => as.every((a) => type.is(a)),
+      name,
+    ),
+    memberType: type,
+  }
 }
