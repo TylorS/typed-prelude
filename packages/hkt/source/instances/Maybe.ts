6075d1b391344eeaec17ec35bb67954c1fe25eb5
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-07-25 21:26:20 -0400

    feat: add more instances for Maybe

--- packages/hkt/source/instances/Maybe.ts
+++ packages/hkt/source/instances/Maybe.ts
@@ -1,5 +1,5 @@
-import { ap, chain, Just, map, Maybe, race } from '@typed/maybe'
-import { Alt, Monad, TypeParams } from 'hkt-ts'
+import { ap, chain, Just, map, Maybe, Nothing, race, unpack } from '@typed/maybe'
+import { Alt, Alternative, Extend, Filterable, Foldable, Monad, TypeParams } from 'hkt-ts'
 
 export const MaybeUri = '@typed/maybe' as const
 export type MaybeUri = typeof MaybeUri
@@ -14,11 +14,30 @@ declare module 'hkt-ts' {
   }
 }
 
-export const maybe: Monad<MaybeUri> & Alt<MaybeUri> = {
+export const maybe: Monad<MaybeUri> &
+  Alt<MaybeUri> &
+  Alternative<MaybeUri> &
+  Foldable<MaybeUri> &
+  Extend<MaybeUri> &
+  Filterable<MaybeUri> = {
   URI: MaybeUri,
   map,
   alt: race,
   chain,
   ap,
   of: Just.of,
+  zero: () => Nothing,
+  reduce: (f, a, m) =>
+    unpack(
+      (b) => f(a, b),
+      () => a,
+      m,
+    ),
+  extend: (f, m) => Just.of(f(m)),
+  filter: (p, m) =>
+    unpack(
+      (a) => (p(a) ? Just.of(a) : Nothing),
+      () => Nothing,
+      m,
+    ),
 }
